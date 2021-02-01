26321de501621bfc0bac4d590b95c49413009874
tyler <tyler.pressnell86@gmail.com>
2021-02-01 17:55:31 -0600

    Began adding game logic and allowing the computer to select a weapon.

--- app/src/main/java/com/example/lab2a/MainActivity.java
+++ app/src/main/java/com/example/lab2a/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.lab2a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.lab2a.Weapon.PAPER;
+import static com.example.lab2a.Weapon.ROCK;
+import static com.example.lab2a.Weapon.SCISSORS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView scoreCounter = (TextView) findViewById(R.id.scoreCounter);
+        scoreCounter.setText("Player: 0, Computer: 0");
+        TextView player = (TextView) findViewById(R.id.playerChoice);
+        player.setText("Player's Weapon: ");
+        TextView computer = (TextView) findViewById(R.id.comChoice);
+        computer.setText("Computer's Weapon");
+    }
+
+    private Weapon playerWeapon;
+    private Weapon comWeapon;
+    int numChoices = 3;
+    int comChoice;
+    int playerScore = 0;
+    int comScore = 0;
+    Random rand = new Random();
+
+    public void onClickRock(){
+        playerWeapon = ROCK;
+        comChoice = rand.nextInt(numChoices);
+
+        if(comChoice == 0){
+            comWeapon = ROCK;
+        }
+        else if(comChoice == 1){
+            comWeapon = PAPER;
+        }
+        else if(comChoice == 2){
+            comWeapon = SCISSORS;
+        }
+
+
+        if(comWeapon == ROCK){
+            TextView winMessage = (TextView) findViewById(R.id.winMessage);
+            winMessage.setText("The Match is a draw!");
+        }
+        else if(comWeapon == PAPER){
+            TextView winMessage = (TextView) findViewById(R.id.winMessage);
+            winMessage.setText("The Computer has won!");
+        }
+        else if(comWeapon == SCISSORS){
+            TextView winMessage = (TextView) findViewById(R.id.winMessage);
+            winMessage.setText("The Player has won!");
+        }
+    }
+
+    public void onClickPaper(){
+        playerWeapon = PAPER;
+    }
+
+    public void onClickScissors(){
+        playerWeapon = SCISSORS;
     }
 }
